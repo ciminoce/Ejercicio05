fc9163aaf6fd8ef752908760d600359cbc968968
Carlos Cimino <ciminoce@yahoo.com.ar>
2023-05-02 19:03:22 -0300

    Agregar archivos de proyecto.

---
+++ Ejercicio05.Consola/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+double radio, perimetro, supercie;
+Console.Write("Ingrese el valor del radio de la circunferencia:");
+radio=double.Parse(Console.ReadLine());
+perimetro = 2 * radio * Math.PI;
+supercie = Math.PI * Math.Pow(radio, 2);
+Console.WriteLine($"Una circ de radio {radio} tiene un perímetro de {perimetro} y una superficie de {supercie}");
